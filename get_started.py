ea8d4234e3a68186db01171022b03f5afad9c695
Jan Randolph Emmanuel <103882365+BeginnerBoi1@users.noreply.github.com>
2025-05-24 22:53:33 +0800

    chore: remove some lines to improve readability

--- get_started.py
+++ get_started.py
@@ -1,23 +1,6 @@
-# -*- coding: utf-8 -*-
-
-################################################################################
-## Form generated from reading UI file 'designerQMLKRy.ui'
-##
-## Created by: Qt User Interface Compiler version 6.9.0
-##
-## WARNING! All changes made in this file will be lost when recompiling UI file!
-################################################################################
-
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFrame, QHBoxLayout, QLabel,
-    QMainWindow, QMenuBar, QSizePolicy, QSpacerItem,
-    QStatusBar, QToolButton, QVBoxLayout, QWidget)
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
 
 from login import *
 # import images_rc  // Comment muna if di pa available ang images_rc
