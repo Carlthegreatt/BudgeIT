f40ddfb812fec296377ac6f60d2dc8cd38bc9540
Erikaux <asioerika12@gmail.com>
2025-06-08 01:34:56 +0800

    add BudgetManager class (w/ transax, expense, income, data manager)

--- assets/backend/BUdgetManager.py
+++ assets/backend/BUdgetManager.py
@@ -4,6 +4,8 @@ from datetime import datetime
 from typing import Optional, Dict, List, Union
 import mysql.connector
 from mysql.connector import Error
+from matplotlib import pyplot as plt
+from collections import defaultdict
 
 
 @dataclass
@@ -279,17 +281,6 @@ class BudgetManager:
         """Destructor to ensure connection is closed"""
         self.close()
 
-
-
-'''
-HEHE copy paste nalangs muna (may binago ako sa get_graph)
-'''
-
-
-
-from datetime import datetime
-from matplotlib import pyplot as plt
-from collections import defaultdict
 
 class DataManager:
     def __init__(self, budget_manager: BudgetManager):
